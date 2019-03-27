b106bab50a3a0a942a2bd0239206e33cf3f1cf84
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-27 21:57:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.map {|season, holiday|
     season_alter1 = season.to_s
     final_seasons = season_alter1.capitalize
-    final_seasons
+    return final_seasons
   }
     
   holiday_hash.map {|season, holiday|
